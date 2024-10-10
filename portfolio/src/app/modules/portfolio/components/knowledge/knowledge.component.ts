868f7e73c28c6581b84d5df4f5f43bde7998f97d
Francis Santiago <francis1505@gmail.com>
2024-10-10 12:14:40 -0300

    :sparkles: Knowledge

---
+++ portfolio/src/app/modules/portfolio/components/knowledge/knowledge.component.ts
@@ -0,0 +1,24 @@
+import { Component, signal } from '@angular/core';
+
+// Interface
+import { IKnowledge } from '../../interface/IKnowledge.interface';
+
+@Component({
+  selector: 'app-knowledge',
+  standalone: true,
+  imports: [],
+  templateUrl: './knowledge.component.html',
+  styleUrl: './knowledge.component.scss'
+})
+export class KnowledgeComponent {
+  public arrayKnowledge = signal<IKnowledge[]>([
+    {
+      src: 'assets/icons/knowledge/html5.svg',
+      alt: 'Ícone de conhecimento em HTML5'
+    },
+    {
+      src: 'assets/icons/knowledge/css3.svg',
+      alt: 'Ícone de conhecimento em CSS3'
+    },
+  ]);
+}
